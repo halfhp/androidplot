09bb62eccc80618582c7b091d2beac72a54d7c5b
Nick Fellows <halfhp@gmail.com>
2011-04-28 03:15:57 +0000

    ANDROIDPLOT-86 - Added missing file.  (was breaking the build)
    git-svn-id: file:///Volumes/androidplot-backup/local-svn/ANDROIDPLOT/trunk@71 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ AndroidPlot-Core/src/main/java/com/androidplot/util/ZIndexable.java
@@ -0,0 +1,77 @@
+package com.androidplot.util;
+
+import java.util.List;
+
+/**
+ * Encapsulates the concept of z-indexable objects;  Each object is stored above or below each other object and may
+ * be moved up and down in the queue relative to other elements in the hash or absolutely to the front or back of the queue.
+ *
+ * Note that the method names correspond to the order of items drawn directly on top of one another using an iterator;
+ * the first element drawn (lowest z-index) is effectively the "bottom" element.
+ * @param <ElementType>
+ */
+public interface ZIndexable<ElementType> {
+
+    /**
+     * Move above all other elements
+     * @param element
+     * @return
+     */
+    public boolean moveToTop(ElementType element);
+
+
+    /**
+     * Move above the specified element
+     * @param objectToMove
+     * @param reference
+     * @return
+     */
+    public boolean moveAbove(ElementType objectToMove, ElementType reference);
+
+
+    /**
+     * Move beneath the specified element
+     *
+     * @param objectToMove
+     * @param reference
+     * @return
+     */
+    public boolean moveBeneath(ElementType objectToMove, ElementType reference);
+
+    /**
+     * Move beneath all other elements
+     * @param key
+     * @return
+     */
+    public boolean moveToBottom(ElementType key);
+
+
+    /**
+     * Move up by one element
+     * @param key
+     * @return
+     */
+    public boolean moveUp(ElementType key);
+
+    /**
+     * Move down by one element
+     * @param key
+     * @return
+     */
+    public boolean moveDown(ElementType key);
+
+    public List<ElementType> elements();
+
+
+    /**
+     * Add beneath all other elements
+     * @param element
+     */
+    //public void addToBottom(ElementType element);
+
+    /**
+     * Add above all other elements
+     * @param element
+     */
+    //public void addToTop(ElementType element);
+}

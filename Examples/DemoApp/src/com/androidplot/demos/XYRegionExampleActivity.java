e6748e103db6e1321959ff507c4d1cb4a5e6d8c0
Nick Fellows <halfhp@gmail.com>
2012-08-11 18:14:13 +0000

    ANDROIDPLOT-133 - Cleaned up XYRegionExample a bit.
    git-svn-id: file:///Volumes/androidplot-backup/local-svn/ANDROIDPLOT/trunk@122 e6edf6fb-f266-4316-afb4-e53d95876a76

--- Examples/DemoApp/src/com/androidplot/demos/XYRegionExampleActivity.java
+++ Examples/DemoApp/src/com/androidplot/demos/XYRegionExampleActivity.java
@@ -29,17 +29,23 @@
 package com.androidplot.demos;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import com.androidplot.series.XYSeries;
 import com.androidplot.ui.*;
 import com.androidplot.util.PaintUtils;
 import com.androidplot.xy.*;
 
 import java.util.Arrays;
 
-
+/**
+ * Demonstrates the usage of RectRegion.
+ */
 public class XYRegionExampleActivity extends Activity {
     private static final int FONT_LABEL_SIZE = 13;
     private XYPlot plot;
@@ -52,18 +58,119 @@ public class XYRegionExampleActivity extends Activity {
     private LineAndPointFormatter lpFormatter3;
     private LineAndPointFormatter lpFormatter4;
 
+    private XYSeries s1;
+    private XYSeries s2;
+    private XYSeries s3;
+    private XYSeries s4;
+
+    private CheckBox s1CheckBox;
+    private CheckBox s2CheckBox;
+    private CheckBox s3CheckBox;
+    private CheckBox s4CheckBox;
+
+    private CheckBox r1CheckBox;
+    private CheckBox r2CheckBox;
+    private CheckBox r3CheckBox;
+    private CheckBox r4CheckBox;
+    private CheckBox r5CheckBox;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.xyregion_example);
         plot = (XYPlot) findViewById(R.id.xyRegionExamplePlot);
+        s1CheckBox = (CheckBox) findViewById(R.id.s1CheckBox);
+        s1CheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                onS1CheckBoxClicked();
+            }
+        });
+
+        s2CheckBox = (CheckBox) findViewById(R.id.s2CheckBox);
+        s2CheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                onS2CheckBoxClicked();
+            }
+        });
+
+        s3CheckBox = (CheckBox) findViewById(R.id.s3CheckBox);
+        s3CheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                onS3CheckBoxClicked();
+            }
+        });
+
+        s4CheckBox = (CheckBox) findViewById(R.id.s4CheckBox);
+        s4CheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                onS4CheckBoxClicked();
+            }
+        });
+
+        r1CheckBox = (CheckBox) findViewById(R.id.r1CheckBox);
+        r2CheckBox = (CheckBox) findViewById(R.id.r2CheckBox);
+        r3CheckBox = (CheckBox) findViewById(R.id.r3CheckBox);
+        r4CheckBox = (CheckBox) findViewById(R.id.r4CheckBox);
+        r5CheckBox = (CheckBox) findViewById(R.id.r5CheckBox);
+
         seriesSetup();
         markerSetup();
         axisLabelSetup();
         regionSetup();
         makePlotPretty();
+        plot.disableAllMarkup();
     }
 
-        /**
+    private void onS1CheckBoxClicked() {
+        if(s1CheckBox.isChecked()) {
+            showS1();
+            r1CheckBox.setEnabled(true);
+            r2CheckBox.setEnabled(true);
+        } else {
+            plot.removeSeries(s1);
+            r1CheckBox.setEnabled(false);
+            r2CheckBox.setEnabled(false);
+        }
+        plot.redraw();
+    }
+
+    private void onS2CheckBoxClicked() {
+        if(s2CheckBox.isChecked()) {
+            showS2();
+            r3CheckBox.setEnabled(true);
+            r4CheckBox.setEnabled(true);
+            r5CheckBox.setEnabled(true);
+        } else {
+            plot.removeSeries(s2);
+            r3CheckBox.setEnabled(false);
+            r4CheckBox.setEnabled(false);
+            r5CheckBox.setEnabled(false);
+        }
+        plot.redraw();
+    }
+
+    private void onS3CheckBoxClicked() {
+        if(s3CheckBox.isChecked()) {
+            showS3();
+        } else {
+            plot.removeSeries(s3);
+        }
+        plot.redraw();
+    }
+
+    private void onS4CheckBoxClicked() {
+        if(s4CheckBox.isChecked()) {
+            showS4();
+        } else {
+            plot.removeSeries(s4);
+        }
+        plot.redraw();
+    }
+
+    /**
      * Cleans up the plot's general layout and color scheme
      */
     private void makePlotPretty() {
@@ -78,12 +185,14 @@ public class XYRegionExampleActivity extends Activity {
         bgPaint.setAlpha(140);
 
         plot.getLegendWidget().setBackgroundPaint(bgPaint);
+
         // adjust the padding of the legend widget to look a little nicer:
         plot.getLegendWidget().setPadding(10, 1, 1, 1);
 
         // adjust the legend size so there is enough room
         // to draw the new legend grid:
-        plot.getLegendWidget().setSize(new SizeMetrics(70, SizeLayoutType.ABSOLUTE, 80, SizeLayoutType.ABSOLUTE));
+        plot.getLegendWidget().setSize(
+            new SizeMetrics(70, SizeLayoutType.ABSOLUTE, 80, SizeLayoutType.ABSOLUTE));
 
         // reposition the grid so that it rests above the bottom-left
         // edge of the graph widget:
@@ -100,33 +209,52 @@ public class XYRegionExampleActivity extends Activity {
         plot.getRangeLabelWidget().setVisible(false);
 
         plot.getGraphWidget().setRangeLabelHorizontalOffset(-1);
+
+        // add enough space to ensure range value labels arent cut off on the left/right:
         plot.getGraphWidget().setRangeLabelWidth(25);
-        plot.getGraphWidget().setDomainLabelWidth(10);
+
+        // add enough space to make sure domain value labels arent cut off on the bottom:
+        plot.getGraphWidget().setDomainLabelWidth(15);
+
         plot.getGraphWidget().setDomainLabelVerticalOffset(-6);
         plot.setBackgroundPaint(null);
         plot.getGraphWidget().setBackgroundPaint(null);
         plot.setBorderPaint(null);
         plot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
-        //plot.getGraphWidget().getDomainLabelPaint().setTextSize(FONT_LABEL_SIZE);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getDomainLabelWidget().getLabelPaint(), GlobalDefs.PLOT_DOMAIN_LABEL_FONT_SIZE_DP);
-        //plot.getGraphWidget().getDomainOriginLabelPaint().setTextSize(FONT_LABEL_SIZE);
-        //PaintUtils.setFontSizeDp(getApplicationContext(), plot.getGraphWidget().getDomainOriginLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
-        //plot.getGraphWidget().getRangeLabelPaint().setTextSize(FONT_LABEL_SIZE);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getRangeLabelWidget().getLabelPaint(), GlobalDefs.PLOT_RANGE_LABEL_FONT_SIZE_DP);
-        //plot.getGraphWidget().getRangeOriginLabelPaint().setTextSize(FONT_LABEL_SIZE);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getGraphWidget().getRangeOriginLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getGraphWidget().getRangeLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getGraphWidget().getDomainOriginLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getGraphWidget().getDomainLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
-        plot.getGraphWidget().getGridLinePaint().setPathEffect(new DashPathEffect(new float[]{1, 2, 1, 2}, 0));
-        //plot.getTitleWidget().getLabelPaint().setTextSize(FONT_LABEL_SIZE);
-        PaintUtils.setFontSizeDp(getApplicationContext(), plot.getTitleWidget().getLabelPaint(), GlobalDefs.PLOT_TITLE_FONT_SIZE_DP);
 
-        //plot.setBackgroundPaint(null);
-	    //plot.getGraphWidget().setBackgroundPaint(null);
-	    //plot.getGraphWidget().setGridBackgroundPaint(null);
+        Context ctx = getApplicationContext();
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getDomainLabelWidget().getLabelPaint(), GlobalDefs.PLOT_DOMAIN_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getRangeLabelWidget().getLabelPaint(), GlobalDefs.PLOT_RANGE_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getGraphWidget().getRangeOriginLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getGraphWidget().getRangeLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getGraphWidget().getDomainOriginLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getGraphWidget().getDomainLabelPaint(), GlobalDefs.PLOT_TICK_LABEL_FONT_SIZE_DP);
+        PaintUtils.setFontSizeDp(ctx,
+            plot.getTitleWidget().getLabelPaint(), GlobalDefs.PLOT_TITLE_FONT_SIZE_DP);
+        plot.getGraphWidget().getGridLinePaint().setPathEffect(new DashPathEffect(new float[]{1, 2, 1, 2}, 0));
         plot.getTitleWidget().pack();
-        //plot.disableAllMarkup();
+    }
+
+    private void showS1() {
+        plot.addSeries(s1, lpFormatter1);
+    }
+
+    private void showS2() {
+        plot.addSeries(s2, lpFormatter2);
+    }
+
+    private void showS3() {
+        plot.addSeries(s3, lpFormatter3);
+    }
+
+    private void showS4() {
+        plot.addSeries(s4, lpFormatter4);
     }
 
     /**
@@ -145,10 +273,10 @@ public class XYRegionExampleActivity extends Activity {
         lpFormatter1.setVertexPaint(null);
         lpFormatter1.getLinePaint().setShadowLayer(0, 0, 0, 0);
 
+        s1 = new SimpleXYSeries(Arrays.asList(series1Numbers),
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "S1");
 
-        plot.addSeries(new SimpleXYSeries(Arrays.asList(series1Numbers),
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
-                "S1"), lpFormatter1);
+        showS1();
 
         // SERIES #2:
         lpFormatter2 = new LineAndPointFormatter(
@@ -159,9 +287,11 @@ public class XYRegionExampleActivity extends Activity {
         lpFormatter2.setFillPaint(null);
         lpFormatter2.setVertexPaint(null);
         lpFormatter2.getLinePaint().setShadowLayer(0, 0, 0, 0);
-        plot.addSeries(new SimpleXYSeries(Arrays.asList(series2Numbers),
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
-                "S2"), lpFormatter2);
+
+        s2 = new SimpleXYSeries(Arrays.asList(series2Numbers),
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "S2");
+
+        showS2();
 
         // SERIES #3:
         lpFormatter3 = new LineAndPointFormatter(
@@ -171,9 +301,11 @@ public class XYRegionExampleActivity extends Activity {
         lpFormatter3.setFillPaint(null);
         lpFormatter3.setVertexPaint(null);
         lpFormatter3.getLinePaint().setShadowLayer(0, 0, 0, 0);
-        plot.addSeries(new SimpleXYSeries(Arrays.asList(series3Numbers),
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
-                "S3"), lpFormatter3);
+
+        s3 = new SimpleXYSeries(Arrays.asList(series3Numbers),
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "S3");
+
+        showS3();
 
         // SERIES #4:
         lpFormatter4 = new LineAndPointFormatter(
@@ -183,9 +315,10 @@ public class XYRegionExampleActivity extends Activity {
         lpFormatter4.setFillPaint(null);
         lpFormatter4.setVertexPaint(null);
         lpFormatter4.getLinePaint().setShadowLayer(0, 0, 0, 0);
-        plot.addSeries(new SimpleXYSeries(Arrays.asList(series4Numbers),
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
-                "S4"), lpFormatter4);
+        s4 = new SimpleXYSeries(Arrays.asList(series4Numbers),
+            SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,"S4");
+        showS4();
+
         plot.setTicksPerRangeLabel(3);
         plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, 1);
     }

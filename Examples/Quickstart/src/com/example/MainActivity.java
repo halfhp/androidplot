b07faec7565a130e802a41e85be3a7caea2d2a7c
Nick Fellows <halfhp@gmail.com>
2012-08-18 22:32:57 +0000

    ANDROIDPLOT-131 - Added Quickstart module to provide a simple AndroidPlot project skeleton to new users of the API.
    git-svn-id: file:///Volumes/androidplot-backup/local-svn/ANDROIDPLOT/trunk@128 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ Examples/Quickstart/src/com/example/MainActivity.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2012 AndroidPlot.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.example;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.series.XYSeries;
+import com.androidplot.xy.*;
+import java.util.Arrays;
+
+/**
+ * The simplest possible example of using AndroidPlot to plot some data.
+ */
+public class MainActivity extends Activity
+{
+
+    private XYPlot mySimpleXYPlot;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        // initialize our XYPlot reference:
+        mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                "Series1");                             // Set the display title of the series
+
+        // same as above
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 200, 0),                   // line color
+                Color.rgb(0, 100, 0),                   // point color
+                null);                                  // fill color (none)
+
+        // add a new series' to the xyplot:
+        mySimpleXYPlot.addSeries(series1, series1Format);
+
+        // same as above:
+        mySimpleXYPlot.addSeries(series2,
+                new LineAndPointFormatter(Color.rgb(0, 0, 200), Color.rgb(0, 0, 100), null));
+
+
+        // reduce the number of range labels
+        mySimpleXYPlot.setTicksPerRangeLabel(3);
+
+        // by default, AndroidPlot displays developer guides to aid in laying out your plot.
+        // To get rid of them call disableAllMarkup():
+        mySimpleXYPlot.disableAllMarkup();
+    }
+}

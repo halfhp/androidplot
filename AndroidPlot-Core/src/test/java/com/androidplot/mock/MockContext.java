985d37a83503ebb2c26642d94d2ebaeba0264d85
Nick Fellows <halfhp@gmail.com>
2014-04-18 15:43:17 -0700

    Downrev'd SDK back to 4.1 due to the lack of a more recent version of the Android SDK being unavailable in Maven:
    http://stackoverflow.com/questions/19859424/why-there-is-no-android-sdk-4-4-in-the-maven-repository
    
    Also uprev the Maven android plugin to 3.8.0 to resolve this issue with newer versions of Maven:
    
    http://stackoverflow.com/questions/17665456/maven-3-1-0-breaks-google-app-engine-maven-plugin

--- AndroidPlot-Core/src/test/java/com/androidplot/mock/MockContext.java
+++ AndroidPlot-Core/src/test/java/com/androidplot/mock/MockContext.java
@@ -20,20 +20,15 @@ import android.content.*;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.UserHandle;
-import android.view.Display;
-import mockit.Instantiation;
 import mockit.Mock;
 import mockit.MockClass;
 
@@ -149,18 +144,8 @@ public class MockContext {
         }
 
         @Override
-        public File[] getExternalFilesDirs(String type) {
-            return new File[0];  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
         public File getObbDir() {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public File[] getObbDirs() {
-            return new File[0];  //To change body of implemented methods use File | Settings | File Templates.
         }
 
         @Override
@@ -171,11 +156,6 @@ public class MockContext {
         @Override
         public File getExternalCacheDir() {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public File[] getExternalCacheDirs() {
-            return new File[0];  //To change body of implemented methods use File | Settings | File Templates.
         }
 
         @Override
@@ -299,21 +279,6 @@ public class MockContext {
         }
 
         @Override
-        public void sendBroadcastAsUser(Intent intent, UserHandle user) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void sendBroadcastAsUser(Intent intent, UserHandle user, String receiverPermission) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void sendOrderedBroadcastAsUser(Intent intent, UserHandle user, String receiverPermission, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
         public void sendStickyBroadcast(Intent intent) {
             //To change body of implemented methods use File | Settings | File Templates.
         }
@@ -325,21 +290,6 @@ public class MockContext {
 
         @Override
         public void removeStickyBroadcast(Intent intent) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void sendStickyBroadcastAsUser(Intent intent, UserHandle user) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void sendStickyOrderedBroadcastAsUser(Intent intent, UserHandle user, BroadcastReceiver resultReceiver, Handler scheduler, int initialCode, String initialData, Bundle initialExtras) {
-            //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public void removeStickyBroadcastAsUser(Intent intent, UserHandle user) {
             //To change body of implemented methods use File | Settings | File Templates.
         }
 
@@ -470,16 +420,6 @@ public class MockContext {
 
         @Override
         public Context createPackageContext(String s, int i) throws PackageManager.NameNotFoundException {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public Context createConfigurationContext(Configuration overrideConfiguration) {
-            return null;  //To change body of implemented methods use File | Settings | File Templates.
-        }
-
-        @Override
-        public Context createDisplayContext(Display display) {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
         }
     }

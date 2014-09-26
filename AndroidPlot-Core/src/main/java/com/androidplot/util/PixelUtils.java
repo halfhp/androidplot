38d014c741a05f42bcd6b1af3e074189080877db
Nick Fellows <halfhp@gmail.com>
2014-09-26 00:01:38 -0500

    ANDROIDPLOT-194 Fixed issue pertaining to parsing negative dimensions with Configurator.

--- AndroidPlot-Core/src/main/java/com/androidplot/util/PixelUtils.java
+++ AndroidPlot-Core/src/main/java/com/androidplot/util/PixelUtils.java
@@ -18,7 +18,6 @@ package com.androidplot.util;
 
 import android.content.Context;
 import android.graphics.PointF;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
@@ -32,21 +31,13 @@ import java.util.regex.Pattern;
 public class PixelUtils {
     private static DisplayMetrics metrics;
     private static final float FLOAT_INT_AVG_NUDGE = 0.5f;
-    //private static float SCALE = 1;   //  pix per dp
-    //private static int X_PIX = 1;     // total display horizontal pix
-    //private static int Y_PIX = 1;     // total display vertical pix
 
     /**
      * Recalculates scale value etc.  Should be called when an application starts or
      * whenever the screen is rotated.
      */
     public static void init(Context ctx) {
-        //DisplayMetrics dm = ctx.getResources().getDisplayMetrics();
-        //SCALE = dm.density;
-        //X_PIX = dm.widthPixels;
-        //Y_PIX = dm.heightPixels;
         metrics = ctx.getResources().getDisplayMetrics();
-
     }
 
     public static PointF add(PointF lhs, PointF rhs) {
@@ -56,21 +47,6 @@ public class PixelUtils {
     public static PointF sub(PointF lhs, PointF rhs) {
         return new PointF(lhs.x - rhs.x, lhs.y - rhs.y);
     }
-
-    /**
-     * Converts a sub-pixel accurate RectF to a Rect
-     * using the closest matching full pixel vals.  This is
-     * useful for clipping operations etc.
-     * @param rectIn The rect to be converted
-     * @return
-     */
-    /*public static Rect toRect(RectF rectIn) {
-        return new Rect(
-                (int) (rectIn.left + FLOAT_INT_AVG_NUDGE),
-                (int) (rectIn.top + FLOAT_INT_AVG_NUDGE),
-                (int) (rectIn.right + FLOAT_INT_AVG_NUDGE),
-                (int) (rectIn.bottom + FLOAT_INT_AVG_NUDGE));
-    }*/
 
     /**
      * Converts a sub-pixel accurate RectF to
@@ -98,8 +74,6 @@ public class PixelUtils {
      * @return Pixel value of dp.
      */
     public static float dpToPix(float dp) {
-        //return SCALE * dp + FLOAT_INT_AVG_NUDGE;
-        //InternalDimension id = new InternalDimension(dp, TypedValue.COMPLEX_UNIT_DIP);
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics);
 
     }
@@ -140,7 +114,7 @@ public class PixelUtils {
      * CODE BELOW IS ADAPTED IN PART FROM MINDRIOT'S SAMPLE CODE HERE:
      * http://stackoverflow.com/questions/8343971/how-to-parse-a-dimension-string-and-convert-it-to-a-dimension-value
      */
-    // -- Initialize dimension string to constant lookup.
+    // Initialize dimension string to constant lookup.
     public static final Map<String, Integer> dimensionConstantLookup = initDimensionConstantLookup();
 
     private static Map<String, Integer> initDimensionConstantLookup() {
@@ -155,47 +129,36 @@ public class PixelUtils {
         return Collections.unmodifiableMap(m);
     }
 
-    // -- Initialize pattern for dimension string.
-    private static final Pattern DIMENSION_PATTERN = Pattern.compile("^\\s*(\\d+(\\.\\d+)*)\\s*([a-zA-Z]+)\\s*$");
-
-    /*public static int stringToDimensionPixelSize(String dimension, DisplayMetrics metrics) {
-        // -- Mimics TypedValue.complexToDimensionPixelSize(int data, DisplayMetrics metrics).
-        InternalDimension internalDimension = stringToInternalDimension(dimension);
-        final float value = internalDimension.value;
-        final float f = TypedValue.applyDimension(internalDimension.unit, value, metrics);
-        final int res = (int) (f + 0.5f);
-        if (res != 0) return res;
-        if (value == 0) return 0;
-        if (value > 0) return 1;
-        return -1;
-    }*/
+    // Initialize pattern for dimension string.
+    protected static final String DIMENSION_REGEX = "^\\-?\\s*(\\d+(\\.\\d+)*)\\s*([a-zA-Z]+)\\s*$";
+    protected static final Pattern DIMENSION_PATTERN = Pattern.compile(DIMENSION_REGEX);
 
     public static float stringToDimension(String dimension) {
-        // -- Mimics TypedValue.complexToDimension(int data, DisplayMetrics metrics).
+        // Mimics TypedValue.complexToDimension(int data, DisplayMetrics metrics).
         InternalDimension internalDimension = stringToInternalDimension(dimension);
         return TypedValue.applyDimension(internalDimension.unit, internalDimension.value, metrics);
     }
 
     private static InternalDimension stringToInternalDimension(String dimension) {
-        // -- Match target against pattern.
+        // Match target against pattern.
         Matcher matcher = DIMENSION_PATTERN.matcher(dimension);
         if (matcher.matches()) {
-            // -- Match found.
-            // -- Extract value.
+            // Match found.
+            // Extract value.
             float value = Float.valueOf(matcher.group(1));
-            // -- Extract dimension units.
+            // Extract dimension units.
             String unit = matcher.group(3).toLowerCase();
-            // -- Get Android dimension constant.
+            // Get Android dimension constant.
             Integer dimensionUnit = dimensionConstantLookup.get(unit);
             if (dimensionUnit == null) {
-                // -- Invalid format.
+                // Invalid format.
                 throw new NumberFormatException();
             } else {
-                // -- Return valid dimension.
+                // Return valid dimension.
                 return new InternalDimension(value, dimensionUnit);
             }
         } else {
-            // -- Invalid format.
+            // Invalid format.
             throw new NumberFormatException();
         }
     }
@@ -209,8 +172,6 @@ public class PixelUtils {
             this.unit = unit;
         }
     }
-
-
 }
 
 
---
+++ AndroidPlot-Core/src/test/java/com/androidplot/util/PixelUtilsTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2014 AndroidPlot.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.androidplot.util;
+
+import mockit.Mockit;
+import org.junit.Test;
+import java.util.regex.Pattern;
+import static junit.framework.Assert.assertTrue;
+import com.androidplot.mock.MockContext;
+
+public class PixelUtilsTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+        Mockit.setUpMocks(MockContext.class);
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testDimensionPattern() {
+        Pattern DIMENSION_PATTERN = Pattern.compile(PixelUtils.DIMENSION_REGEX);
+        assertTrue("Dimension failed dimension pattern match", DIMENSION_PATTERN.matcher("20dp").matches());
+        assertTrue("Negative dimension failed dimension pattern match", DIMENSION_PATTERN.matcher("-20dp").matches());
+    }
+}

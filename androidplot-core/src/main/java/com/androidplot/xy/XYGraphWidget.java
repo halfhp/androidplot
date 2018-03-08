07ff4bca5db0223fbc667ad2a6ae1a369c3717ac
Jonathan Jackson <jjackson@innomatix.biz>
2018-03-08 16:45:12 -0600

    algabraic changes to for loop termination statements
    the new code should be matamatically equivalent to the previous version

--- androidplot-core/src/main/java/com/androidplot/xy/XYGraphWidget.java
+++ androidplot-core/src/main/java/com/androidplot/xy/XYGraphWidget.java
@@ -635,7 +635,7 @@ public class XYGraphWidget extends Widget {
         // draw lines LEFT of origin:
         double xPix;
         final float gridRectLeftMinusFudge = gridRect.left - FUDGE;
-        for (int i = ONE; domainOriginPix - (i * domainStep.getStepPix()) >= gridRectLeftMinusFudge; i++) {
+        for (int i = ONE; domainOriginPix - gridRectLeftMinusFudge >= i * domainStep.getStepPix(); i++) {
             xPix = domainOriginPix - (i * domainStep.getStepPix());
             double xVal = domainOrigin.doubleValue() - i * domainStep.getStepVal();
 
@@ -648,7 +648,7 @@ public class XYGraphWidget extends Widget {
 
         // draw lines RIGHT of origin:
         final float gridRectRightPlusFudge = gridRect.right + FUDGE;
-        for (int i = ONE; domainOriginPix + (i * domainStep.getStepPix()) <= gridRectRightPlusFudge; i++) {
+        for (int i = ONE; i * domainStep.getStepPix() <= gridRectRightPlusFudge - domainOriginPix; i++) {
             xPix = domainOriginPix + (i * domainStep.getStepPix());
             double xVal = domainOrigin.doubleValue() + i * domainStep.getStepVal();
 
@@ -684,7 +684,7 @@ public class XYGraphWidget extends Widget {
         // draw lines ABOVE origin:
         double yPix;
         final float gridRectTopMinusFudge = gridRect.top - FUDGE;
-        for (int i = ONE; rangeOriginPix - (i * rangeStepPix) >= gridRectTopMinusFudge; i++) {
+        for (int i = ONE; rangeOriginPix - gridRectTopMinusFudge >= i * rangeStepPix; i++) {
             yPix = rangeOriginPix - (i * rangeStepPix);
             double yVal = rangeOrigin.doubleValue() + i * rangeStep.getStepVal();
 
@@ -697,7 +697,7 @@ public class XYGraphWidget extends Widget {
 
         // draw lines BENEATH origin:
         final float gridRectBottomPlusFudge = gridRect.bottom + FUDGE;
-        for (int i = ONE; rangeOriginPix + (i * rangeStepPix) <= gridRectBottomPlusFudge; i++) {
+        for (int i = ONE; i * rangeStepPix <= gridRectBottomPlusFudge - rangeOriginPix; i++) {
             yPix = rangeOriginPix + (i * rangeStepPix);
             double yVal = rangeOrigin.doubleValue() - i * rangeStep.getStepVal();
             if (yPix >= gridRect.top) {
